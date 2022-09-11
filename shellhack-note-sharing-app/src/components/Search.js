728b4baa37d9152d78e427a0cbca239f14907864
Abijah Nesbitt <abijah.nez@gmail.com>
2022-09-10 23:56:58 -0400

    Added file picker

--- shellhack-note-sharing-app/src/components/Search.js
+++ shellhack-note-sharing-app/src/components/Search.js
@@ -1,39 +1,24 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { TextField } from '@mui/material'
-import "./Search.css"
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { TextField } from "@mui/material";
+import "./Search.css";
+import Note from "./Note";
 export default function Search() {
   return (
-    <div className='explore-container'>
-        <div className='s-bar'>
-            <TextField id="outlined-basic" label="Search for Notes" variant="outlined" style={{width: '100%', marginBottom: '30px'}} />
-            <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
-        alt="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-        </div>
+    <div className="explore-container">
+      <div className="s-bar">
+        <TextField
+          id="outlined-basic"
+          label="Search for Notes"
+          variant="outlined"
+          style={{ width: "100%", marginBottom: "30px" }}
+        />
+      </div>
     </div>
-  )
+  );
 }
--- shellhack-note-sharing-app/src/components/Tabs.js
+++ shellhack-note-sharing-app/src/components/Tabs.js
@@ -1,16 +1,41 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
+
+import { useState } from "react";
 import Tab from "@mui/material/Tab";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
 import TabPanel from "@mui/lab/TabPanel";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 import "./Tabs.css";
-import { Button } from "@mui/material";
+import { Button, Divider, TextField } from "@mui/material";
 import Note from "./Note";
+import { FilePicker } from "react-file-picker";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function Tabs() {
   const [value, setValue] = React.useState("1");
-
+  const [open, setOpen] = useState(false);
+  const [file, setFile] = useState("");
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
@@ -24,7 +49,10 @@ export default function Tabs() {
             <Tab label="Recieved Notes" value="3" />
           </TabList>
           <div>
-            <Button style={{ backgroundColor: "green", color: "white" }}>
+            <Button
+              style={{ backgroundColor: "green", color: "white" }}
+              onClick={handleOpen}
+            >
               + Add a Note
             </Button>
           </div>
@@ -48,6 +76,103 @@ export default function Tabs() {
           </div>
         </TabPanel>
       </TabContext>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        style={{ overflowY: "scroll" }}
+      >
+        <Box sx={style}>
+          <h2 style={{ margin: "0", marginBottom: "20px" }}>
+            Add Your Note(s)
+          </h2>
+          <Divider />
+
+          <Button
+            style={{
+              backgroundColor: "green",
+              color: "white",
+              marginTop: "20px",
+              marginBottom: "20px",
+            }}
+            component="label"
+          >
+            Upload a File
+            <input
+              type="file"
+              hidden
+              onChange={(e) => {
+                setFile(e.target.files[0]);
+              }}
+            />
+          </Button>
+          <p>{file.name}</p>
+          <Divider />
+          <p>Name Your Note(s)</p>
+          <TextField placeholder="Name" />
+          <p>Describe Your Note(s)</p>
+          <TextField
+            placeholder="Describe"
+            multiline
+            rows={"4"}
+            style={{ marginBottom: "20px" }}
+          />
+          <Divider />
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <FormControl>
+              <FormLabel>
+                <p>Choose Visibility</p>
+              </FormLabel>
+              <RadioGroup
+                aria-labelledby="demo-radio-buttons-group-label"
+                defaultValue="female"
+                name="radio-buttons-group"
+                row
+                style={{ marginBottom: "20px" }}
+              >
+                <FormControlLabel
+                  value="public"
+                  control={<Radio />}
+                  label="Public"
+                />
+                <FormControlLabel
+                  value="private"
+                  control={<Radio />}
+                  label="Private"
+                />
+              </RadioGroup>
+            </FormControl>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Button
+              style={{
+                backgroundColor: "white",
+                color: "red",
+                border: "red 1px solid",
+              }}
+              onClick={handleClose}
+            >
+              Cancel
+            </Button>
+            <Button
+              style={{
+                backgroundColor: "Blue",
+                color: "white",
+              }}
+            >
+              Add Note
+            </Button>
+          </div>
+        </Box>
+      </Modal>
     </div>
   );
 }
